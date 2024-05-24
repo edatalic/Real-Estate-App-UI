6b6c7af3e2d0bffab3b84b038b8adf3466ac4306
Eda Talıç <edatalic@gmail.com>
2024-05-24 17:02:22 +0300

    Real Estate App UI
    React real estate responsive UI design project with HTML, CSS, and React Map.

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import HomePage from "./routes/homePage/homePage.jsx"
+import { createBrowserRouter,RouterProvider } from "react-router-dom"
+import ListPage from "./routes/listPage/listPage.jsx"
+import Layout from "./routes/layout/layout.jsx"
+import SinglePage from "./routes/singlePage/singlePage.jsx";
+import ProfilePage from "./routes/profilePage/profilePage.jsx";
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/list",
+          element: <ListPage />
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />
+        }
+      ]
+    }
+  ])
+
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
